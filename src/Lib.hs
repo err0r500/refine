30a64637f45b6423b53e42e1132833dfec47f402
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2020-04-23 15:13:35 +0000

    add revision repo to Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,37 +9,44 @@ import qualified Control.Monad.Catch           as Catch
 
 import qualified Adapter.Http.Router           as HttpRouter
 import qualified Adapter.InMemory.NodeRepo     as InMem
+import qualified Adapter.InMemory.RevisionRepo as InMem
 import qualified Adapter.Logger                as Logger
 import qualified Config.Config                 as Config
 import qualified Usecase.InsertRevision        as UC
 import qualified Usecase.Interactor            as UC
 import qualified Usecase.LogicHandler          as UC
 
-
-type NodeStore = TVar InMem.NodeStore
-
-newtype InMemoryApp a = InMemoryApp
-    { unApp :: ReaderT NodeStore IO a
-    } deriving (Applicative, Functor, Monad, Catch.MonadThrow, Catch.MonadCatch, MonadReader NodeStore, MonadIO)
-
-run :: NodeStore -> InMemoryApp a -> IO a
-run state app = runReaderT (unApp app) state
-
-getFreshState :: (MonadIO m) => m NodeStore
-getFreshState = newTVarIO $ InMem.NodeStore mempty
-
 start :: IO ()
 start = do
         putStrLn "== Refine =="
-        state  <- getFreshState
+        state  <- freshState
         router <- HttpRouter.start (logicHandler interactor) $ run state
         port   <- Config.getIntFromEnv "PORT" 3000
         putStrLn $ "starting server on port: " ++ tshow port
         Warp.run port router
 
+
+type State = (TVar InMem.NodeStore, TVar InMem.RevisionStore)
+
+newtype InMemoryApp a = InMemoryApp
+    { unApp :: ReaderT State IO a
+    } deriving (Functor, Applicative, Monad, Catch.MonadThrow, Catch.MonadCatch, MonadReader State, MonadIO)
+
+
+run :: State -> InMemoryApp a -> IO a
+run state app = runReaderT (unApp app) state
+
+freshState :: (MonadIO m) => m State
+freshState = do
+        nodes     <- newTVarIO $ InMem.NodeStore mempty
+        revisions <- newTVarIO $ InMem.RevisionStore mempty
+        return (nodes, revisions)
+
 interactor :: UC.Interactor InMemoryApp
-interactor = UC.Interactor { UC.nodeRepo_ = nodeRepo }
-        where nodeRepo = UC.NodeRepo InMem.insertNode InMem.getNodeContentByHash
+interactor = UC.Interactor { UC.nodeRepo_ = nodeRepo, UC.revisionRepo_ = revisionRepo }
+    where
+        nodeRepo     = UC.NodeRepo InMem.insertNode InMem.getNodeContentByHash
+        revisionRepo = UC.RevisionRepo InMem.insertRevision
 
 logicHandler :: UC.Interactor InMemoryApp -> UC.LogicHandler InMemoryApp
 logicHandler i =
